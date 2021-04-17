db15c00166e051402f19a026fdc54847fe9118eb
root <root@DESKTOP-E6BINCG.localdomain>
2021-04-16 20:51:31 -0400

    check

--- background.js
+++ background.js
@@ -3,7 +3,8 @@ function responseMsg(msg) {
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete' && tab.active) {
+    // if (changeInfo.status == 'complete' && tab.active) {
+    if (tab.active) {
         chrome.tabs.sendMessage(tabId, {
             cmd: 'pasteEmail'
         }, responseMsg);
